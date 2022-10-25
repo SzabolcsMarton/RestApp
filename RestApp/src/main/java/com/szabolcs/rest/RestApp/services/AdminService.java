55902894f32b35173a969447ed9fd8528da5e9c7
SzabolcsMarton <maszabolcs@gmail.com>
2022-10-25 11:56:43 +0200

    add activateBasicEmployee to ManagerService and some creation method to AdminService

--- RestApp/src/main/java/com/szabolcs/rest/RestApp/services/AdminService.java
+++ RestApp/src/main/java/com/szabolcs/rest/RestApp/services/AdminService.java
@@ -3,6 +3,8 @@ package com.szabolcs.rest.RestApp.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.szabolcs.rest.RestApp.model.Department;
+import com.szabolcs.rest.RestApp.model.Role;
 import com.szabolcs.rest.RestApp.repositories.DepartmentRepository;
 import com.szabolcs.rest.RestApp.repositories.EmployeeRepository;
 import com.szabolcs.rest.RestApp.repositories.RoleRepository;
@@ -17,9 +19,15 @@ public class AdminService {
     private DepartmentRepository departmentRepository;
     
     @Autowired
-    private RoleRepository repository;
+    private RoleRepository roleRepository;
     
+    public Role createNewRole(Role role) {
+	return roleRepository.save(role);
+    }
     
+    public Department createNewDepartment(Department dep) {
+	return departmentRepository.save(dep);
+    }
     
     
 }
--- RestApp/src/main/java/com/szabolcs/rest/RestApp/services/ManagerService.java
+++ RestApp/src/main/java/com/szabolcs/rest/RestApp/services/ManagerService.java
@@ -1,5 +1,7 @@
 package com.szabolcs.rest.RestApp.services;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,7 @@ import com.szabolcs.rest.RestApp.exceptions.DepartmentNotFoundException;
 import com.szabolcs.rest.RestApp.exceptions.EmployeeNotFoundException;
 import com.szabolcs.rest.RestApp.model.Department;
 import com.szabolcs.rest.RestApp.model.Employee;
+import com.szabolcs.rest.RestApp.model.Role;
 import com.szabolcs.rest.RestApp.repositories.DepartmentRepository;
 import com.szabolcs.rest.RestApp.repositories.EmployeeRepository;
 import com.szabolcs.rest.RestApp.repositories.RoleRepository;
@@ -21,11 +24,15 @@ public class ManagerService {
     @Autowired
     private DepartmentRepository departmentRepository;
     
-    public Employee setUpBasicEmployee(Long employeeId,Long departmentId, int salary ) {
+    public Employee activateBasicEmployee(Long employeeId,Long departmentId, int salary, String roleName ) {
 	Employee emp = employeeRepository.findById(employeeId).orElseThrow(()-> new EmployeeNotFoundException("Cannot find employee with id: " + employeeId));
 	Department dep = departmentRepository.findById(departmentId).orElseThrow(()-> new DepartmentNotFoundException("Cannot find department with id: " + departmentId));
+	Role role = roleRepository.findByRole(roleName.toUpperCase());
 	emp.setDepartment(dep);
 	emp.setSalary(salary);
+	emp.setActivated(true);
+	emp.addRole(role);
+	emp.setActivationDate(LocalDate.now());
 	return employeeRepository.save(emp);
 	
     }
