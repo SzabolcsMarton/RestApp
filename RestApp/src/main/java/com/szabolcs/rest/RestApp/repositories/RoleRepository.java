c43577a54f6d6304c37c451af78b6018a4dcf240
SzabolcsMarton <maszabolcs@gmail.com>
2022-10-25 11:49:46 +0200

    add findbyrole query

--- RestApp/src/main/java/com/szabolcs/rest/RestApp/repositories/RoleRepository.java
+++ RestApp/src/main/java/com/szabolcs/rest/RestApp/repositories/RoleRepository.java
@@ -1,9 +1,14 @@
 package com.szabolcs.rest.RestApp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.szabolcs.rest.RestApp.model.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    
+    @Query("select r from Role r where r.role = :role")
+    Role findByRole(@Param("role") String role);
 
 }
